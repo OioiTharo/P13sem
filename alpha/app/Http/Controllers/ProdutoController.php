122db8cd0af9aa6cabff46f658192ba3443d9983
oioitharo <thais.randrade5@senacsp.edu.br>
2023-10-18 18:03:16 -0300

    subindo

---
+++ alpha/app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produto;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    public function index()
+    {
+        return view('produtos.index', ['produtos' => Produto::All()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Produto $produto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Produto $produto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Produto $produto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Produto $produto)
+    {
+        //
+    }
+}
---
+++ alpha/app/Models/Produto.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+    use HasFactory;
+    protected $fillable = ['PRODUTO_NOME',  'PRODUTO_DESC', 'PRODUTO_PRECO', 'PRODUTO_DESCONTO', 'CATEGORIA_ID'];
+}
